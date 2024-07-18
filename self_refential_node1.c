40bec1e75226f7bfe0a95d3083eea1cb854a7789
hardik <kantariyahardik618@ggamil.com>
2024-07-18 12:05:37 +0530

    738364

--- self_refential_node1.c
+++ self_refential_node1.c
@@ -33,9 +33,9 @@ int main(){
     b.ptr = &c;
     c.ptr = &d;
     d.ptr = &a;
-    printf("Data of a is: \n%d\t%c",d.ptr->data, d.ptr->ch);
-    printf("Data of b is: \n%d\t%c",a.ptr->data, a.ptr->ch);
-    printf("Data of c is: \n%d\t%c",b.ptr->data, b.ptr->ch);
-    printf("Data of d is: \n%d\t%c",c.ptr->data, c.ptr->ch);
+    printf("Data of a is: %d\t%c",d.ptr->data, d.ptr->ch);
+    printf("\nData of b is: %d\t%c",a.ptr->data, a.ptr->ch);
+    printf("\nData of c is: %d\t%c",b.ptr->data, b.ptr->ch);
+    printf("\nData of d is: %d\t%c",c.ptr->data, c.ptr->ch);
     return 0;
 }
