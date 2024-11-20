a5a91ffc50f74096fbf152bd7673372ac656972b
ZenyPokiya <sq.zeny26@gmail.com>
2024-11-20 22:57:40 +0530

    MASJH

--- array1.c
+++ array1.c
@@ -4,16 +4,20 @@ int main(){
     printf("Enter array size:");
     scanf("%d",&n);
     int arr[n];
+
+    
     for (int i=0;i<n;i++){
         printf("Enter elements of %d:",i);
         scanf("%d",&arr[i]);
  }
  for (int i=0;i<n;i++){
-    printf("%d\t",arr[i]);
+    printf("%d\t",arr[i]) ;
  }
+
+
     printf("\nEnter array size:");
     scanf("Enter array size:");
-    scanf("%d",&k);
+    scanf("%d",&k); 
     int xyz[k];
     for (int j=0;j<k;j++){
         printf("Enter elements of %d:",j);
